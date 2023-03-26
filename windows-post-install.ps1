0c6798f7ad694cf039eced88ff767c065778403e
calle482 <76658989+calle482@users.noreply.github.com>
2023-03-26 13:05:22 +0200

    fixed path

--- windows-post-install.ps1
+++ windows-post-install.ps1
@@ -37,6 +37,6 @@ cd 'c:\Program Files\Git\bin'
 New-Item -ItemType Directory -Force -Path "C:\Program Files (x86)\Steam\userdata/149571724"
 Write-Host "Sleeping to allow git clone to finish"
 Start-Sleep -Seconds 15
-Copy-Item -Recurse "c:\temp\csgo-config\730" "C:\Program Files (x86)\Steam\userdata/149571724\"
+Copy-Item -Recurse "c:\temp\730" "C:\Program Files (x86)\Steam\userdata/149571724\"
 
 Write-Host "Script finsihed"
