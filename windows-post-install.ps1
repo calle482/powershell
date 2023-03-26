a9bfd872afc17dd5dbb47dbeb6d641480cd2adee
calle482 <76658989+calle482@users.noreply.github.com>
2023-03-26 10:02:49 +0200

    yeees

---
+++ windows-post-install.ps1
@@ -0,0 +1,15 @@
+#install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+$programs = @('Discord','Steam','Firefox','powershell-core','vscodium','notepadplusplus.install','vlc','git','7zip','microsoft-windows-terminal','spotify','winscp','geforce-experience')
+
+#install apps with chocolatey
+ForEach-Object ($program in $programs) {
+    choco install $program -y
+}
+
+#Debloat install
+$PSScriptRoot\Windows10Debloater.ps1
+
+#activate windows
+irm https://massgrave.dev/get | iex
