2cd8dd4caf87b5bfb08848b32614cde73654bc00
calle482 <76658989+calle482@users.noreply.github.com>
2023-03-26 10:22:27 +0200

    test

--- windows-post-install.ps1
+++ windows-post-install.ps1
@@ -9,7 +9,8 @@ foreach ($program in $programs) {
 }
 
 #Debloat install
-$PSScriptRoot+"\Windows10Debloater.ps1"
+$script = $PSScriptRoot+"\Windows10Debloater.ps1"
+&$script
 
 #activate windows
 irm https://massgrave.dev/get | iex
